008113ae8f377d7834c425630a6105ad2674f04d
SaiHemanthBR <sh.bheemreddy@icloud.com>
2020-04-02 22:57:28 +0530

    Added Skills and Contact

--- js/mdc-init.js
+++ js/mdc-init.js
@@ -3,3 +3,9 @@ var MDCButtons = document.querySelectorAll('.mdc-button');
 MDCButtons.forEach(function(element, index) {
   mdc.ripple.MDCRipple.attachTo(element);
 })
+
+var MDCIconButtons = document.querySelectorAll('.mdc-icon-button');
+
+MDCIconButtons.forEach(function(element, index) {
+  new mdc.ripple.MDCRipple(document.querySelector('.mdc-icon-button')).unbounded = true;
+})
