1bf6614f2ba6b7414e1a2be632a8479299398749
SaiHemanthBR <sh.bheemreddy@gmail.com>
2021-06-20 23:48:09 +0530

    Added Hero Section

--- src/sections/Hero.tsx
+++ src/sections/Hero.tsx
@@ -1,6 +1,12 @@
+import './styles/Hero.scss'
+
 function Hero() {
   return (
-    <>Hero</>
+    <div className="hero-image" id="hero-image">
+      <div className="hero-text">
+        <div className="hero-title" id="hero-title">Sai Hemanth Bheemreddy</div>
+      </div>
+    </div>
   );
 }
 
