701384bc41e68760c051f5d546894f8d811de07c
SaiHemanthBR <sh.bheemreddy@gmail.com>
2021-06-20 21:59:11 +0530

    Updated App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,17 @@
+import NavBar from './components/NavBar';
 import './styles/App.scss';
 
 function App() {
   return (
     <div className="App">
-      
+      <NavBar />
+      {/* <div className="container">
+        <section className="child">1</section>
+        <section className="child">2</section>
+        <section className="child">3</section>
+        <section className="child">4</section>
+        <section className="child">5</section>
+      </div> */}
     </div>
   );
 }
