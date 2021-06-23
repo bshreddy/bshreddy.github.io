e339d77e9624acbfcd8f49b7495448eaf9ee23ea
SaiHemanthBR <sh.bheemreddy@gmail.com>
2021-06-23 12:46:41 +0530

    Added graphql

--- src/index.tsx
+++ src/index.tsx
@@ -3,22 +3,24 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import reportWebVitals from './vitals/reportWebVitals';
 import { StylesProvider, ThemeProvider } from '@material-ui/core/styles';
+import { ApolloProvider } from "@apollo/client";
+
 import { theme } from './theme';
+import { graphqlClient } from './graphql';
 
 import './styles/index.scss';
 
 ReactDOM.render(
   <ThemeProvider theme={theme}>
     <StylesProvider injectFirst>
-      <React.StrictMode>
-        <App />
-      </React.StrictMode>
+      <ApolloProvider client={graphqlClient}>
+        <React.StrictMode>
+          <App />
+        </React.StrictMode>
+      </ApolloProvider>
     </StylesProvider>
   </ThemeProvider>,
   document.getElementById('root')
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+reportWebVitals(console.log);
