0f4129d3da15a4df4dfc64d145195c5ed4c96f41
SaiHemanthBR <sh.bheemreddy@gmail.com>
2021-08-14 22:20:15 +0530

    Fixed Hero responsive issues

--- src/contents/sections/hero/index.tsx
+++ src/contents/sections/hero/index.tsx
@@ -1,4 +1,5 @@
 import Typography from "@material-ui/core/Typography";
+import Container from '@material-ui/core/Container';
 import { getRandomInt } from 'apps/helpers';
 
 import heros from 'configs/sections/hero/index.json';
@@ -10,11 +11,11 @@ function Hero() {
 
   return (
     <div className="hero-image" id="hero-image" style={{ backgroundImage: `url(${backgroundImage})` }}>
-      <div className="hero-text" style={{ color: hero.color }}>
+      <Container maxWidth="xl" className="hero-text" style={{ color: hero.color }}>
         <Typography variant="h4" component="div" className="hero-title-before">Hello, My name is</Typography>
         <Typography variant="h1" component="div" className="hero-title">Sai Hemanth Bheemreddy</Typography>
         <Typography variant="h4" component="div" className="hero-title-after">I love engineering things for fun!</Typography>
-      </div>
+      </Container>
 
       <div className="hero-footer" style={{ color: hero.color }}>
         <Typography variant="body1" gutterBottom className="name">{hero.name}</Typography>
