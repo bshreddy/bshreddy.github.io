4199cc76b0eb39a812417df775fb20a4ce99286e
SaiHemanthBR <sh.bheemreddy@gmail.com>
2021-08-21 13:21:16 +0530

    Fixed permission issue

--- aws/index.js
+++ aws/index.js
@@ -2,12 +2,14 @@ const gh_graphql = require('./gh_graphql');
 const aws = require('aws-sdk');
 
 exports.handler = async (event) => {
-  const gh_res = await gh_graphql.client.query({query: gh_graphql.githubReposQuery});
+  const gh_res = await gh_graphql.client.query({ query: gh_graphql.githubReposQuery });
 
   const s3 = new aws.S3();
   return await s3.putObject({
-    Bucket : process.env.S3_BUCKET,
-    Key : process.env.S3_KEY,
-    Body : JSON.stringify(gh_res)
+    Bucket: process.env.S3_BUCKET,
+    Key: process.env.S3_KEY,
+    Body: JSON.stringify(gh_res),
+    ACL: 'public-read'
+
   }).promise();
 };
