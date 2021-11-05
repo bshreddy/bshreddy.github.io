825bb0fae97accceed4bac5172f3b2ef6ecba202
SaiHemanthBR <sh.bheemreddy@gmail.com>
2021-11-05 19:06:12 +0530

    Fixed pinned items incorrect order

--- src/apps/sections/projects/index.ts
+++ src/apps/sections/projects/index.ts
@@ -2,20 +2,12 @@ import axios from "axios";
 import { Repository, GithubAPIResponse, ProjectsData } from 'apps/types';
 
 export const get_projects = async () => {
-  const res = (await axios.get<GithubAPIResponse>(process.env.REACT_APP_PROJECTS_URL!));
-  const data = res.data.data;
+  const data = (await axios.get<GithubAPIResponse>(process.env.REACT_APP_PROJECTS_URL!)).data.data;
+  const repositories: {[key: string]: Repository} = data.user.repositories.nodes.reduce((a, x) => ({...a, [x.id]: x}), {})
 
-  const pinned_repos: Repository[] = [];
-  const other_repos: Repository[] = [];
-
-  data.user.repositories.nodes.forEach((node) => {
-    if(data.user.pinnedItems.edges.some((pinned_node) => pinned_node.node.id === node.id)) {
-      pinned_repos.push(node);
-    } else {
-      other_repos.push(node);
-    }
-  });
-
-
-  return {pinned_repos, other_repos} as ProjectsData;
+  return {
+    pinned_repos: data.user.pinnedItems.edges.map((pinned_node) => repositories[pinned_node.node.id]),
+    other_repos: data.user.repositories.nodes
+    .filter((node) => !data.user.pinnedItems.edges.some((pinned_node) => pinned_node.node.id === node.id))
+  } as ProjectsData;
 }
